fc9fa631cb6c0de0b787cc14f899c4b8b7407292
Shahab yazdi <sh.yazdi@gmail.com>
2021-03-04 13:09:06 +0330

    initializing demo site

---
+++ website/gatsby-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  siteMetadata: {
+    title: "React Element Popper",
+  },
+  plugins: [
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+  ],
+};
