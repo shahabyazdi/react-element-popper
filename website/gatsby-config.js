d1d9dea0000f01cf1165998aae5d2ff21c810e74
Shahab yazdi <sh.yazdi@gmail.com>
2021-03-08 17:16:10 +0330

    v1.0.8

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -3,13 +3,13 @@ module.exports = {
     siteUrl: "https://shahabyazdi.github.io/react-element-popper/",
     en: {
       title: "React Element Popper",
-      description: "A small library to create a variety of elements that require Popper, such as dropdowns, modals, multi selects, and more.",
+      description: "A small React component to create a variety of elements that require Popper, such as dropdown menus, modals, multi selects, and more.",
       keywords: ["react", "element", "popper", "component", "position", "dropdown", "modal"],
       type: "website"
     },
     fa: {
       title: "ری اکت المنت پاپر",
-      description: "یک کتابخانه کوچک برای ساخت عناصر متنوعی که به Popper نیاز دارند ، مانند  dropdown ها، modal ها ، multi select ها و غیره .",
+      description: "یک کامپوننت کم حجم ری اکت برای ساخت عناصر متنوعی که به Popper نیاز دارند ، مانند  dropdown ها، modal ها ، multi select ها و غیره .",
       keywords: ["ری اکت", "المنت", "پاپر", "کامپوننت", "منو", "منو کشویی"],
       type: "وب سایت"
     }
